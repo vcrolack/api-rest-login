ee03c73cbe5403d119c95176d2dc72f327c4d092
Víctor Rolack <victorolack@gmail.com>
2021-11-16 22:00:27 -0300

    url eliminada

--- ApiLogin/api/urls.py
+++ ApiLogin/api/urls.py
@@ -3,6 +3,5 @@ from .views import UserView
 
 urlpatterns = [
   path('users/', UserView.as_view(), name='users_list'),
-  path('users/<int:id>', UserView.as_view(), name='user_process'),
   path('users/<str:username>/<str:password>/', UserView.as_view(), name='user_validator'),
 ]
